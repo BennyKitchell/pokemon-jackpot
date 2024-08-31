0421754511d6d329e68004435f8396fc98c802d8
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-08-30 19:31:24 -0500

    Add core folder for business logic and use gin for http framework

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"pokemon-jackpot/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+const port = ":8020"
+
 func main() {
-	println("Server running on port: 8020")
+	println("Server running on port: ", port)
+	_, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.POST("/user", controllers.CreateUser)
+
+	if err := router.Run(port); err != nil {
+		log.Printf("failed to run the server: %v", err)
+	}
 }
