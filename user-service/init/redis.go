7e566a3413c6a9708524baa097a96a981851487a
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-08-31 18:11:59 -0500

    feat: Add redis for init  and docker compose

---
+++ user-service/init/redis.go
@@ -0,0 +1,15 @@
+package initializers
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+func ConnectRedis() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return client
+}
