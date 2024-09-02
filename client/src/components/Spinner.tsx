c6e626ef0e523bb98d7b6e93d2835968b73d170a
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-09-02 15:17:49 -0500

    fix: Add the pizazz for the jackpot with logic

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,5 +1,5 @@
+import ConfettiExplosion from 'react-confetti-explosion';
 
-import './Spinner.css'
 interface Pokemon {
     id: string,
     image_url: string,
@@ -9,25 +9,29 @@ interface Pokemon {
 
 interface SpinnerProps {
     pokemon: Pokemon[]
+    jackpot: boolean
 }
 function Spinner(props: SpinnerProps) {
     const pokemon = props.pokemon;
+    const jackpot = props.jackpot;
     return(
         <>
         {
             <div className="spinner-container">
+            {jackpot && <ConfettiExplosion />}
             {pokemon.length > 0 ? (
                 pokemon.map((pokemon, key) => (
-                  <div key={key} className={"outer-command-container"}>
+                  <div key={key} className={jackpot ? "outer-command-container logo jackpot" :"outer-command-container"}>
                     <div className="pokemon-frame" >
-                    <img src={pokemon.image_url} width={250} height={250} alt="Pokemon Icon" />
+                    <img src={pokemon.image_url} width={250} height={250} alt="React logo" />
                         </div>
                     <div className="pokemon-name">{pokemon.name}</div>
                   </div>
                 ))
               ) : (
-                <h2>Syncing Pokemon...</h2>
+                <h2>Loading Pokemon...</h2>
               )}
+              {jackpot && <ConfettiExplosion />}
               </div>
         }
         </>
